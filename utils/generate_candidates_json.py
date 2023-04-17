5a2318fd5ab1d236e78b91f3e15ce94bae24a0c4
smk762 <smk762@iinet.net.au>
2023-04-18 00:14:48 +0800

    add candidate json generator to gh actions

---
+++ utils/generate_candidates_json.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import os
+import json
+
+script_path = os.path.realpath(os.path.dirname(__file__))
+
+
+def parse_candidates(season):
+    data = {
+        "AR": [],
+        "EU": [],
+        "NA": [],
+        "SH": []
+    }
+    headers = []
+    with open(f"{script_path}/../season{season}/candidates/README.md", "r") as f:
+        for line in f.readlines():
+            print(line)
+            if line.startswith("|") and line.find("--") == -1:
+
+                if len(headers) == 0:
+                    headers = [i.strip() for i in line.split("|")][2:]
+                else:
+                    line_data = [i.strip() for i in line.split("|")][2:]
+                    for i in range(len(line_data)):
+                        if line_data[i].find("]") > -1:
+                            info = line_data[i].split("]")[
+                                1].replace("(", "").replace(")", "")
+                            notary = line_data[i].split("]")[
+                                0].replace("[", "")
+                            region = headers[i]
+                            if len(notary) > 1:
+                                address = info.split(" ")[1].replace('"', '')
+                                print("Generating JSON for",
+                                      notary, region, address)
+
+                                base_notary = notary
+                                if base_notary.endswith("2") and base_notary not in ["mx222"]:
+                                    base_notary = base_notary[:-1]
+                                if base_notary == "alienx":
+                                    base_notary = base_notary[:-1]
+
+                                data[region].append({
+                                    "address": address,
+                                    "candidate": notary,
+                                    "qr_code": f"https://raw.githubusercontent.com/KomodoPlatform/NotaryNodes/master/season7/qr_codes/{notary}_{region}.png",
+                                    "text": f"https://github.com/KomodoPlatform/NotaryNodes/tree/master/season7/candidates/{base_notary}/README.md"
+                                })
+    return data
+
+
+if __name__ == "__main__":
+    data = parse_candidates(7)
+    json.dump(data, open(
+        f"{script_path}/../season7/candidates.json", "w"), indent=4)
